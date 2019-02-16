308ee419ab732e1e93b7df8af437631131ae3f62
Elad Nisan <elad@globalbit.co.il>
2019-02-16 20:17:51 +0200

    done express chpater

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path  = require('path');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText,'sad');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/css',express.static(path.join(__dirname, 'public')));
 
-server.listen(3000);
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=> {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+})
+
+app.listen(3000);
