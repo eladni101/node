9f704bacaa441d6704e391531adb1e3090ee9f1c
Elad Nisan <elad@globalbit.co.il>
2019-04-28 15:01:03 +0300

    add sequliznze to app

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Smadi102'
+const sequelize = new Sequelize('node-complete', 'root', 'Smadi102', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
