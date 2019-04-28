485b51703a2a6532a4aa41b89ed908fae663d0b5
Elad Nisan <elad@globalbit.co.il>
2019-04-28 15:03:19 +0300

    add to dtabase

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'Smadi102', {
   dialect: 'mysql',
-  host: 'localhost'
+  host: 'localhost',
 });
 
 module.exports = sequelize;
